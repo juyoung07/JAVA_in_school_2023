0a337b509aa8e07732d9f6c3716a2acc7248508a
juyoung07 <128597386+juyoung07@users.noreply.github.com>
2023-03-29 21:32:31 +0900

    Add files via upload

---
+++ Test_03.java
@@ -0,0 +1,13 @@
+public class Test_03{
+	public static void main(String args[]) {
+		String group = "규정샘 최고";
+		String name = "품격있으심";
+		
+		//System.out.println(group);
+		//System.out.println(name);
+		
+		System.out.println(group + " " + name);
+	}
+
+
+}
