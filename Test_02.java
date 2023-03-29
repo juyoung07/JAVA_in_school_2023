408860dfd14b91b3dcc38af4355106919fe53e5d
juyoung07 <128597386+juyoung07@users.noreply.github.com>
2023-03-29 21:32:12 +0900

    Add files via upload

---
+++ Test_02.java
@@ -0,0 +1,13 @@
+public class Test_02 {
+	public static void main(String args[]) {
+
+		//System.out.println("품격의 2반~~");
+	
+		int a = 20;
+		int b = 3;
+		int c = a + b;
+		System.out.println(c);
+
+	}
+
+}
