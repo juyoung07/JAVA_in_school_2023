d008f1e5e3d553652d84938e491b6845c61f8b8c
juyoung07 <128597386+juyoung07@users.noreply.github.com>
2023-03-29 21:33:00 +0900

    Add files via upload

---
+++ Quiz_01.java
@@ -0,0 +1,12 @@
+public class Quiz_01{
+	public static void main(String args[]){
+
+		String me = "내가 제일 좋아하는 연예인은?";
+		String you = "규정샘";
+	
+		System.out.println(me + " " + you + " " + "입니다.");
+
+
+	}
+
+}
