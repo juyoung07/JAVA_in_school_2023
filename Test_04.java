53f6706a4abfd9f9bbb5ce13ce1356644737064d
juyoung07 <128597386+juyoung07@users.noreply.github.com>
2023-03-29 21:32:47 +0900

    Add files via upload

---
+++ Test_04.java
@@ -0,0 +1,22 @@
+public class Test_04 {
+	public static void main(String args[]) {
+
+		String str1 = "미스터트롯2";
+		String str2 = "김용필";
+
+		/*System.out.print(str1 + ":");
+		System.out.print(str2);
+		System.out.println();*/
+
+		int a = 48;
+
+		System.out.println(str1 + " " + str2 + "은 " + a + "세 입니다.");
+
+		a = 49;
+		System.out.println("내년에 " + str2 + "은 " + a + "세가 됩니다.");
+
+		
+
+
+	}
+}
